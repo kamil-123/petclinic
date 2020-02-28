0473ddd1a38e775c81ad0ad3d565e710d1e361eb
Kamil Drozd <drozdk@centrum.cz>
2020-02-28 10:57:34 +0100

    routes added, views folders

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,24 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pets','PetController@search');
+Route::get('/pets/index','PetController@index');
+Route::get('/pets/create','PetController@create');
+Route::get('/pets/{id}/edit','PetController@edit');
+Route::get('/pets/{id}', 'PetController@show');
+
+Route::post('/pets/create', 'PetController@store');
+Route::put('/pets/{id}/edit', 'PetController@update');
+
+Route::get('/owners','OwnerController@search');
+Route::get('/owners/index','OwnerController@index');
+Route::get('/owners/create','OwnerController@create');
+Route::get('/owners/{id}/edit','OwnerController@edit');
+Route::get('/owners/{id}', 'OwnerController@show');
+
+Route::post('/owners/create', 'OwnerController@store');
+Route::put('/owners/{id}/edit', 'OwnerController@update');
+
+
+
