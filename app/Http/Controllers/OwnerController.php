bfe9c74229a618b2ed16693cf5e38ce37ba51350
Kamil Drozd <drozdk@centrum.cz>
2020-02-28 11:19:23 +0100

    quick push

--- app/Http/Controllers/OwnerController.php
+++ app/Http/Controllers/OwnerController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 class OwnerController extends Controller
 {
     public function index(){
-        return "hello from index";
+        
+        return 'ahoj';
     }
 }
